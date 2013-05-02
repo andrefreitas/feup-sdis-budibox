faf3dc559a69c0b5bf8498eecb77c669de12cdb4
Ana Gomes <acgg.gomes@gmail.com>
2013-05-02 11:32:12 +0100

    Criada função de geolocaliçao e ir buscar nome. Melhorada funcao de request para login. Adicionados ficheiros watchdog.

--- client/client.py
+++ client/client.py
@@ -1,5 +1,5 @@
 from file import File
-
+import utils
 
 class Client:
     def __init__(self, name, email, password):
@@ -9,18 +9,18 @@ class Client:
         self.api = "http://master.budibox.com/api/"
     
     def login(self):
-        data = 'login.php?auth=12&email="'+self.email+'"&password="'+self.password+'"'
-        self.request(data)
-    
-    def request(self, data):
-        request = urllib.urlopen(self.api + data)
-        json_response = json.load(request) 
-        print json_response['result']
-        if (json_response['result'] == 'error'):
+        url = self.api+'login.php'
+        values = {'auth': '12',
+                  'email': self.email,
+                  'password': self.password
+                  }
+        response = utils.json_request(url, values)
+        print response
+        if (response['result'] == 'error'):
             print "Username or password incorrect. Please try again or register at http://www.budibox.com!"
         else:
             print "Login successful!"
 
-c = Client('ana', 'tt', 'maasd')
+c = Client('ana', 'a', 'a')
 c.login()
      
--- client/utils.py
+++ client/utils.py
@@ -1,10 +1,39 @@
+"""
+ Utils
+ Authors: Ana Gomes, Andre Freitas
+ Description: This is a class with some very useful utilities. 
+"""
+
 import httplib
 import urllib
 import urllib2
 import json
+import platform
+import socket
+import re
 
 def json_request(url,params):
     url += "?"
     for param in params.keys():
         url += param + "=" + params[param] + "&"
     return json.load(urllib.urlopen(url))
+
+def get_computer_name():
+    return platform.node()
+
+def get_location(ip_address):
+    url = 'http://api.hostip.info/get_html.php?ip=' + ip_address + '&position=true'
+    response = urllib.urlopen(url).read()
+    
+    latitude_exp = "(Latitude:).(-?[0-9]+\.+[0-9]+)"
+    latitude = float (re.search(latitude_exp, response).group(2))
+    longitude_exp = "(Longitude:).(-?[0-9]+\.+[0-9]+)"
+    longitude = float (re.search(longitude_exp, response).group(2))
+    
+    location = {'latitude': latitude,
+                'longitude': longitude
+                }
+    return json.dumps(location)
+    
+print get_location('193.136.33.133')
+    
