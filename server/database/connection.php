186eac081bec33ca3b06baa2b651c13fca99b2e2
André Freitas <p.andrefreitas@gmail.com>
2013-05-04 14:46:17 +0100

    Colocado um try catch na ligação do mongodb.

--- server/database/connection.php
+++ server/database/connection.php
@@ -1,6 +1,10 @@
 <?php
     require_once("../configuration.php");
-    $connection = new MongoClient("mongodb:// ". $members . "/?replicaSet=" . $replicaSet);
-    $db = $connection->selectDB($database);
+    try {
+        $connection = new MongoClient("mongodb:// ". $members . "/?replicaSet=" . $replicaSet);
+        $db = $connection->selectDB($database);
+    } catch (Exception $e) {
+        echo "Failed connecting to mongodb";
+    }
     
 ?>
