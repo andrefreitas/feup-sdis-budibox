a1482d3342885bd01ce8a57dcaf2f08f054fe4b2
André Freitas <p.andrefreitas@gmail.com>
2013-05-04 22:48:58 +0100

    Configuração do MongoDB feita com autenticação.

--- server/configuration.php
+++ server/configuration.php
@@ -3,5 +3,7 @@
 $replicaSet = "budibox";
 $members = "artemis.budibox.com:27017,apolo.budibox.com:27017,aura.budibox.com:27017";
 $database = "budibox";
+$databaseUser = "budibox";
+$databasePassword = "i10Mzf1";
 $apikey = "12";
 ?>
--- server/database/connection.php
+++ server/database/connection.php
@@ -1,7 +1,7 @@
 <?php
     require_once("../configuration.php");
     try {
-        $connection = new MongoClient("mongodb:// ". $members . "/?replicaSet=" . $replicaSet);
+        $connection = new MongoClient("mongodb:// ". $members,array("replicaSet" => $replicaSet, "db" => $database, "username" => $databaseUser, "password" => $databasePassword ));
         $db = $connection->selectDB($database);
     } catch (Exception $e) {
         echo "Failed connecting to mongodb: " . $e->getMessage();
