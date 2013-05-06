adfe9dfeb692cfb141a6cf1d763e69e763079a94
André Freitas <p.andrefreitas@gmail.com>
2013-05-06 22:38:58 +0100

    O actions/login.php altera agora a session e redirecciona para a página anterior.

--- server/actions/login.php
+++ server/actions/login.php
@@ -1,6 +1,14 @@
 <?php
 chdir("../common");
+require_once("init.php");
+chdir("../database");
+require_once("users.php");
 if(isset($_GET["email"]) and isset($_GET["password"])){
-    
+    $email = (string) $_GET["email"];
+    $password = (string) $_GET["password"];
+    if(checkUserLogin($email, $password)){
+        $_SESSION = getUser($email);
+    }
+    header("Location: " . $_SERVER['HTTP_REFERER']);
 }
 ?>
