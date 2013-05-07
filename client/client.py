03a054e47e4d4eea65010e82344d081a9c6db4b0
André Freitas <p.andrefreitas@gmail.com>
2013-05-07 18:08:11 +0100

    Merge

--- client/client.py
+++ client/client.py
@@ -1,26 +1,19 @@
-from file import File
+
 import utils
 
 class Client:
-    def __init__(self, name, email, password):
-        self.name = name
+    def __init__(self, email, password):
         self.email = email
         self.password = password
         self.api = "http://master.budibox.com/api/"
     
     def login(self):
         url = self.api+'login.php'
-        values = {'auth': '12',
-                  'email': self.email,
+        values = {'email': self.email,
                   'password': self.password
                   }
         response = utils.json_request(url, values)
         print response
-        if (response['result'] == 'error'):
-            print "Username or password incorrect. Please try again or register at http://www.budibox.com!"
-        else:
-            print "Login successful!"
+        return response
 
-c = Client('ana', 'a', 'a')
-c.login()
      
