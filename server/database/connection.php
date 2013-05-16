101098aa6edadb1838fa7e7bc158efdc765c44e4
André Freitas <p.andrefreitas@gmail.com>
2013-05-16 10:02:38 +0100

    Colocado o primary como read preference.

--- server/database/connection.php
+++ server/database/connection.php
@@ -2,6 +2,7 @@
     require_once("../configuration.php");
     try {
         $connection = new MongoClient("mongodb:// ". $members,array("replicaSet" => $replicaSet, "db" => $database, "username" => $databaseUser, "password" => $databasePassword, "timeout" => 6000 ));
+        $connection->setReadPreference(MongoClient::RP_PRIMARY_PREFERRED , array());
         $db = $connection->selectDB($database);
     } catch (Exception $e) {
         echo "Failed connecting to mongodb: " . $e->getMessage();
