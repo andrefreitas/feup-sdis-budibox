0b140ae38d22fbdc5c42b3dbd0e6f6b974853cc1
André Freitas <p.andrefreitas@gmail.com>
2013-05-16 10:07:11 +0100

    Organizado o código da connection.

--- server/database/connection.php
+++ server/database/connection.php
@@ -1,7 +1,12 @@
 <?php
     require_once("../configuration.php");
     try {
-        $connection = new MongoClient("mongodb:// ". $members,array("replicaSet" => $replicaSet, "db" => $database, "username" => $databaseUser, "password" => $databasePassword, "timeout" => 6000 ));
+        $connection = new MongoClient("mongodb:// ". $members,
+        array("replicaSet" => $replicaSet, 
+              "db" => $database, 
+              "username" => $databaseUser, 
+              "password" => $databasePassword,
+              "timeout" => 6000 ));
         $connection->setReadPreference(MongoClient::RP_PRIMARY_PREFERRED , array());
         $db = $connection->selectDB($database);
     } catch (Exception $e) {
