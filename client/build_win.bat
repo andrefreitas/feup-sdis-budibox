c62dcaa86e2d9f2ac553556ba1a8c915cf6e94ba
André Freitas <p.andrefreitas@gmail.com>
2013-05-25 19:20:11 +0100

    Clean build win

--- client/build_win.bat
+++ client/build_win.bat
@@ -1,1 +1,6 @@
 cxfreeze client_daemon.py --target-dir dist --include-modules atexit,PySide.QtNetwork --icon qml/budibox.ico
+copy qml dist
+copy db_setup.sql dist
+cd dist
+sqlite3 data.db < db_setup.sql
+del db_setup.sql
