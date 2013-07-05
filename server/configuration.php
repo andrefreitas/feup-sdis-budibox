c5bc54eba40cac0b8c357bcba0eae9d04cf742f4
André Freitas <p.andrefreitas@gmail.com>
2013-07-05 10:42:45 +0100

    Comentadas as configurações do replicaset para se adpatar ao facto de que só temos um servidor agora

--- server/configuration.php
+++ server/configuration.php
@@ -2,6 +2,7 @@
 /* Database */
 $replicaSet = "budibox";
 $members = "artemis.budibox.com:27017,apolo.budibox.com:27017,aura.budibox.com:27017";
+$databaseServer = "185.11.166.119:27017";
 $database = "budibox";
 $databaseUser = "budibox";
 $databasePassword = "i10Mzf1";
--- server/database/connection.php
+++ server/database/connection.php
@@ -1,6 +1,7 @@
 <?php
     require_once("../configuration.php");
     try {
+        /*
         $connection = new MongoClient("mongodb:// ". $members,
         array("replicaSet" => $replicaSet, 
               "db" => $database, 
@@ -8,6 +9,11 @@
               "password" => $databasePassword,
               "timeout" => 6000 ));
         $connection->setReadPreference(MongoClient::RP_PRIMARY_PREFERRED , array());
+        */
+        $config = array("db" => $database,
+                        "username" => $databaseUser,
+                        "password" => $databasePassword);
+        $connection = new MongoClient("mongodb://" . $databaseServer, $config );
         $db = $connection->selectDB($database);
     } catch (Exception $e) {
         echo "Failed connecting to mongodb: " . $e->getMessage();
